ad2aef82dd5d5980fce94ccf8b81b96d0f8bf871
intmainvoidreturn0 <execvpe@disroot.org>
2024-11-22 16:30:54 +0900

    Initial commit

---
+++ boldText.ts
@@ -0,0 +1,30 @@
+import { addPreSendListener, removePreSendListener } from "@api/MessageEvents";
+import definePlugin from "@utils/types";
+
+let bold = false;
+
+const BoldText = () => {
+    addPreSendListener((_, message) => {
+        if (message.content.match("!Boldtext")) {
+            bold = !bold;
+            //message.content = message.content.replace("!Boldtext", "").trim();
+        }
+        if (bold) {
+            message.content = "# " + message.content;
+        }
+    });
+};
+
+export default definePlugin({
+    authors: [
+        {
+            name: "intmainvoidreturn0",
+            id: 1291234047909171221n,
+        },
+    ],
+    dependencies: ["MessageEventsAPI"],
+    description: "Bold Texts",
+    name: "BoldText",
+    start: BoldText,
+    stop: () => removePreSendListener(BoldText),
+});
